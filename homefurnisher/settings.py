cbdda0694bd7e3d3b494d14d856f8729cbadac6a
Xtrialandtest <1trialandtest@gmail.com>
2024-03-12 16:15:05 +0300

    second commit

--- homefurnisher/settings.py
+++ homefurnisher/settings.py
@@ -123,12 +123,12 @@ WSGI_APPLICATION = "homefurnisher.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 DATABASES["default"]=dj_database_url.parse(getenv("DJ_DATABASE_URL"))
 

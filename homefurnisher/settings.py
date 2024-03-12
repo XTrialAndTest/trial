20455049df2107cc69784c39585a0ed8917a32e2
Xtrialandtest <1trialandtest@gmail.com>
2024-03-12 16:13:39 +0300

    second commit

--- homefurnisher/settings.py
+++ homefurnisher/settings.py
@@ -15,13 +15,12 @@ if path.isfile(dotenv_file):
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z3tn^hig@_v5z-!830_qjrjl$qi8-a$b6+d+5bnoi^0b9=d-zf'
+
 SECRET_KEY = getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 DEBUG=getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS=getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS=getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
@@ -124,12 +123,12 @@ WSGI_APPLICATION = "homefurnisher.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 DATABASES["default"]=dj_database_url.parse(getenv("DJ_DATABASE_URL"))
 
